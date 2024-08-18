0ef853465df8982ba016d1808599641ed8455b45
Abhijeet120303 <140697735+Abhijeet120303@users.noreply.github.com>
2024-08-18 08:06:40 +0530

    Update ApiGatewayMain.java

--- hospital-management/api-gateway/src/main/java/org/dnyanyog/ApiGatewayMain.java
+++ hospital-management/api-gateway/src/main/java/org/dnyanyog/ApiGatewayMain.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiGatewayMain {
 
   public static void main(String[] args) {
-    SpringApplication.run(ApiGatewayMain.class, args)
-      
-      
-      
-      ;
+    SpringApplication.run(ApiGatewayMain.class, args);
   }
 }
